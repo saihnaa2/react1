6da67bad7203d78c754c7b1352c1cd9f4ffc32f6
hongorzul <hongorzulrose@gmail.com>
2022-09-13 16:54:30 +0800

    123
    123

--- 1app/source/screens/home.js
+++ 1app/source/screens/home.js
@@ -1,6 +1,15 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import WheelOfFortune from 'react-native-wheel-of-fortune'
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+import WheelOfFortune from 'react-native-wheel-of-fortune';
+
 const participants = [
   '%10',
   '%20',
@@ -12,41 +21,116 @@ const participants = [
   '%90',
   'FREE',
 ];
-const wheelOptions = {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winnerValue: null,
+      winnerIndex: null,
+      started: false,
+    };
+    this.child = null;
+  }
+
+  buttonPress = () => {
+    this.setState({
+      started: true,
+    });
+    this.child._onPress();
+  };
+
+  render() {
+    const wheelOptions = {
       rewards: participants,
-      knobSize: 50,
+      knobSize: 30,
       borderWidth: 5,
-      borderColor: '#000',
-      innerRadius: 50,
-      duration: 4000,
+      borderColor: '#fff',
+      innerRadius: 30,
+      duration: 6000,
       backgroundColor: 'transparent',
       textAngle: 'horizontal',
-      knobSource: require('./assets/images/knoob.png'),
-      getWinner: (value, index) => {
-        this.setState({winnerValue: value, winnerIndex: index});
-      },
+      knobSource: require('../../assets/knob.png'),
       onRef: ref => (this.child = ref),
     };
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title=" Details"
-        onPress={() => navigation.navigate('Details')}/>
-      <Button title="About" onPress={()=>navigation.navigate('About')}/>
-      <Button title="Settings" onPress={()=>navigation.navigate('Settings')}/>
-      <WheelOfFortune
-    wheelOptions={wheelOptions}
-/>
-   
-    </View>
-    
-    
-  );
-  
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle={'light-content'} />
+        <WheelOfFortune
+          options={wheelOptions}
+          getWinner={(value, index) => {
+            this.setState({winnerValue: value, winnerIndex: index});
+          }}
+        />
+        {!this.state.started && (
+          <View style={styles.startButtonView}>
+            <TouchableOpacity
+              onPress={() => this.buttonPress()}
+              style={styles.startButton}>
+              <Text style={styles.startButtonText}>Spin to win!</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        {this.state.winnerIndex != null && (
+          <View style={styles.winnerView}>
+            <Text style={styles.winnerText}>
+              You win {participants[this.state.winnerIndex]}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({winnerIndex: null});
+                this.child._tryAgain();
+              }}
+              style={styles.tryAgainButton}>
+              <Text style={styles.tryAgainText}>TRY AGAIN</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    );
+  }
 }
+export default App;
 
-export default HomeScreen;
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E74C3C'
+  },
+  startButtonView: {
+    position: 'absolute',
+  },
+  startButton: {
+    backgroundColor: 'rgba(0,0,0,.5)',
+    marginTop: 50,
+    padding: 5,
+  },
+  startButtonText: {
+    fontSize: 50,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  winnerView: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tryAgainButton: {
+    padding: 10,
+  },
+  winnerText: {
+    fontSize: 30,
+  },
+  tryAgainButton: {
+    padding: 5,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  tryAgainText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  
+});

2fbe0bbce97362beaa91071bf8ab9318e0f28638
hongorzul <hongorzulrose@gmail.com>
2022-09-27 16:22:37 +0800

    uptade3

--- 2app/App.js
+++ 2app/App.js
@@ -1,16 +1,22 @@
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  function GoalInputText(enteredText) {
+    console.log(enteredText);
+  }
+
+  function addGoalInputText() {}
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textinput}
           placeholder="chinii zorilgo"
+          onChangeText={GoalInputText}
         ></TextInput>
         <Button title="zorilgo nemeh"></Button>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>zorilgiin jigsaalt</Text>
       </View>
     </View>
@@ -19,18 +25,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
-    //margin: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textinput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    with: "80%",
+    with: "70%",
     marginRight: 8,
-    //padding: 8
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });

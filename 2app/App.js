57bb094414af788a2ed091f4b6ba2c197b3e327f
hongorzul <hongorzulrose@gmail.com>
2022-09-27 16:27:24 +0800

    uptade3

--- 2app/App.js
+++ 2app/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [] =useState('')
   function GoalInputText(enteredText) {
     console.log(enteredText);
   }
